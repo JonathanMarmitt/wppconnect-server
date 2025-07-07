7fbfd31f108ba4a978b7d9ca225789bef39afc68
Jonathan Marmitt <jmarmitt.br@gmail.com>
2025-07-07 10:40:04 -0300

    Back to true

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export default {
   port: '21465',
   deviceName: 'Vittality',
   poweredBy: 'WPPConnect-Server',
-  startAllSession: false,
+  startAllSession: true,
   tokenStoreType: 'file',
   maxListeners: 0,
   customUserDataDir: './userDataDir/',
